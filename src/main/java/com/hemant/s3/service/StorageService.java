104bcc33e5c95742c19823d59c44040c90e522aa
hemant sahu <hemantjava90@gmail.com>
2021-05-28 18:15:37 +0530

    Initial commit

---
+++ src/main/java/com/hemant/s3/service/StorageService.java
@@ -0,0 +1,9 @@
+package com.hemant.s3.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+    public String uploadFile(MultipartFile file);
+    public byte[] downloadFile(String fileName);
+    public String deleteFile(String fileName);
+}
---
+++ src/test/java/com/hemant/s3/FileStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemant.s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
